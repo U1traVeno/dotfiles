569337d6461d60382ef6a115441da4f03b05333d
veno <happ1less1917@gmail.com>
2026-07-15 06:17:56 +0800

    add ffmpeg to media.nix and move node.nix to modules/packages/

--- hosts/thinkpad.nix
+++ hosts/thinkpad.nix
@@ -2,11 +2,12 @@
 {
   imports = [
     ../modules/shell
-    ../modules/node.nix
     ../modules/packages/base.nix
     ../modules/packages/modern-unix.nix
     ../modules/packages/modern-tui.nix
+    ../modules/packages/media.nix
     ../modules/packages/agents.nix
+    ../modules/packages/node.nix
     ../modules/packages/python.nix
     ../modules/packages/golang.nix
   ];
